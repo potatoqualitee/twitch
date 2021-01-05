f4e0b5e6f5e97d3c93558418911db56395a02909
Chrissy LeMaire <clemaire@gmail.com>
2021-01-05 20:11:16 +0100

    Revert "nice lil fixes"
    This reverts commit 0b4caa40f7f8701a70fd86b9e4920629bb17c122.

--- tvbot/public/Connect-TvServer.ps1
+++ tvbot/public/Connect-TvServer.ps1
@@ -72,9 +72,9 @@ function Connect-TvServer {
             $nick = "NICK $botname"
 
             # enble extra features from twitch
-            #$capabilities = "CAP REQ :twitch.tv/membership twitch.tv/tags twitch.tv/commands"
+            $capabilities = "CAP REQ :twitch.tv/membership twitch.tv/tags twitch.tv/commands"
 
-            Send-Server -Message $auth, $nick
+            Send-Server -Message $auth, $nick, $capabilities
         } catch {
             throw $_
         }
